82c9f7dad2f8771712a3e85f0d684c3eb40d1789
Costa-Giacomo <92380404+Costa-Giacomo@users.noreply.github.com>
2021-11-11 19:03:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
 #include <iostream>
 using namespace std;
-
 int main() {
-   return 0;
+    int n=0, x1=0, x2=1, x3=0;
+    cin >> n;
+    if(n==1){
+    cout << "1" << endl;
+    cout << "1" << endl;
+    }
+    if(n>1){
+        for(int i=0; i<n+1; i++){
+            x1=x2;
+            x2=x3;
+            x3=x1+x2;
+            if (x3<=n){
+                cout << x3 << endl;
+            }else{
+                break;
+            }
+        }
+    }
+    return 0;
 }
